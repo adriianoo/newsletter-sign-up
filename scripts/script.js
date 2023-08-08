612f6efb7cb13b649cccb6c5466cd7bb6f8e8fae
Adriano Jimenez Hidalgo <adrijimenez@hotmail.com>
2023-08-08 15:06:06 +0200

    added js features

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,37 @@
+const subscribeButton = document.getElementById("subscribe-button");
+const dismissButton = document.getElementById("dismiss-button");
+const confirmationMessage = document.getElementById("confirmation-message");
+const typedEmail = document.getElementById("typed-email");
+
+function displaySuccessMessage() {
+    document.querySelector(".success-message").style.display = "flex";
+}
+
 function validation() {
     let form = document.getElementById("form");
     let input = document.getElementById("input").value;
-    let alert = document.getElementById("text");
+    let text = document.getElementById("text");
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
     if (input.match(pattern)) {
         form.classList.add("valid");
         form.classList.remove("invalid");
         text.innerHTML = "";
-    }
+
+        typedEmail.textContent = input;
+        subscribeButton.addEventListener("click", displaySuccessMessage);
+    } 
+    
     else {
         form.classList.remove("valid");
         form.classList.add("invalid");
         text.innerHTML = "Valid email required";
         text.style.color = "#ff0000";
+
+        subscribeButton.removeEventListener("click", displaySuccessMessage);
     }
 }
+
+dismissButton.addEventListener("click", function () {
+    location.reload();
+});
